df94117994525a5389c0a55a53c76970aaf762bf
norogoth <norogoth@gmail.com>
2021-07-05 15:42:28 -0700

    more updates

--- src/App.js
+++ src/App.js
@@ -7,16 +7,39 @@ import Card from './Card.js';
 
 function App() {
 
+	const navData = [
+		{
+			label:"home",
+			link:"null for now",
+		},
+		{
+			label:"about",
+			link:"null for now"
+		}
+	];
+
 	const cardData = [
 		{
 			imagePath: "null",
-			cardTitle: "sample title",
-			cardDescription: "sample description. lorem ipsum dolor sit amet.",
+			title: "sample title",
+			description: "sample description. lorem ipsum dolor sit amet.",
 		}
 	];
 	
-	const cards = cardData.map((cardObj) => {
-		return <Card imagePath={cardObj.imagePath} title={cardObj.itle} description={cardObj.cardDescription} />
+	const cards = cardData.map((cardObj, i) => {
+		console.log("entering code");
+		if (i % 3 === 0) {
+		console.log("3");
+			return <Card className="card cardStyle3" imagePath={cardObj.imagePath} title={cardObj.title} description={cardObj.description} />
+		}
+		else if (i % 2 === 0) {
+		console.log("2");
+			return <Card className="card cardStyle2" imagePath={cardObj.imagePath} title={cardObj.title} description={cardObj.description} />
+		}
+		else {
+		console.log("1");
+			return <Card className="card cardStyle1" imagePath={cardObj.imagePath} title={cardObj.title} description={cardObj.description} />
+		}
 	});
 
   return (
@@ -31,8 +54,8 @@ function App() {
 		</div>
 		<Navbar>
 			<Nav>
-				<Nav.Link className="navLink">Home</Nav.Link>
-				<Nav.Link className="navLink">About</Nav.Link>
+				<Nav.Link className="navLink" id="homeNav">Home</Nav.Link>
+				<Nav.Link className="navLink" id="aboutNav">About</Nav.Link>
 			</Nav>
 		  </Navbar>
 		  <div className="cardDiv">
--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,6 @@
 export default function Card(props) {
 	return (
-		<div className="card">
+		<div className={props.className}>
 			<img src={props.imagePath} className="cardImage"/>
 			<h2 className="cardTitle">{props.title}</h2>
 			<p className="cardDescription">{props.description}</p>
