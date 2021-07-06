5132453013c0a462cd8012ea845ab11a3dac3f5d
norogoth <norogoth@gmail.com>
2021-07-06 16:48:29 -0700

    fixed that weird margin issue on the side

--- src/About.js
+++ src/About.js
@@ -5,9 +5,13 @@ export default function About() {
 		<div className="about">
 			<div className="articleContent">
 				<Sidebar />
+				<img alt="image of a good employee" className="selfPortrait" src="./img/me.jpeg"/>
 				<h2 className="articleHeading">Who I Am</h2>
 				<p className="aboutText">
 					My name is Landon Aaker and I have been working on software development for three years. I especially love working with SQL, Linux, and any problems that can be solved within the CLI. I have been specializing lately in full-stack web-development but I also like coding in Python, C# and (I guess) Java. I love all things IT and I look forward towards creating solutions for your company.
+				</p>
+				<p>
+					When I am not programming, I am often reading old books, baking bread, or planning out my next D&D campaign.
 				</p>
 			</div>
 		</div>
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,21 +5,21 @@ export default function Sidebar() {
 				<h3 className="sidebarHeading">Email</h3>
 				<p className="sideBarContent">norogoth@gmail.com</p>
 				<h3 className="sidebarHeading">Education</h3>
-		<ul className="schoolList">
-			<li className="schoolPar">
-				<p className="school">CSU San Bernardino</p>
-				<p className="degree">Bachelors in History</p>
-			</li>
-			<li className="schoolPair">
-				<p className="school">Grand Canyon University</p>
-				<p className="degree">Masters in Education</p>
-			</li>
-			<li className="schoolPair">
-				<p className="school">Western Governor's University</p>
-				<p className="degree" id="bscs">👉Bachelors in Computer Science</p>
-			</li>
-		</ul>
-			</div>
+			<ul className="schoolList">
+				<li className="schoolPar">
+					<p className="school">CSU San Bernardino</p>
+					<p className="degree">Bachelors in History</p>
+				</li>
+				<li className="schoolPair">
+					<p className="school">Grand Canyon University</p>
+					<p className="degree">Masters in Education</p>
+				</li>
+				<li className="schoolPair">
+					<p className="school">Western Governor's University</p>
+					<p className="degree" id="bscs">👉Bachelors in Computer Science</p>
+				</li>
+			</ul>
+		</div>
 		</div>
 	)
 }
