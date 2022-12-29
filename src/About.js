7492783fac2049c957ed198f5030b214c5c0ad0d
norogoth <norogoth@gmail.com>
2022-12-29 09:14:17 -0800

    updating bio

--- src/About.js
+++ src/About.js
@@ -8,10 +8,10 @@ export default function About() {
 				<img alt="image of a good software developer." className="selfPortrait" src="./img/me.jpeg"/>
 				<h2 className="articleHeading">Who I Am</h2>
 				<p className="aboutText">
-					My name is Landon Aaker and I have been working on software development for three years. I especially love working with SQL, Linux (I use Pop OS), and any problems that can be solved within the CLI. I have been specializing lately in full-stack web-development but I also like coding in Python, C# and Java. I love all things IT and I look forward towards creating solutions for you.
+					My name is Landon Aaker and I have been working on software development for three years. I especially love working with SQL, Linux (Debian flavors). In my current position I have been using a LAMP(erl) stack for backend (with some nodeJS) and JQuery and Backbone for a little front-end. I also like coding in C#, Python, and Java. I love all things IT and I look forward towards creating solutions for you.
 				</p>
 				<p className="aboutText">
-					When I am not programming, I am often reading old books, baking bread, or planning out my next D&D campaign.
+					When I am not programming, I am often reading old books, spending time with my baby, or planning out my next Dungeon World (like D&D but better) campaign.
 					I also like <a href="https://www.poetryfoundation.org/poems/44651/the-tide-rises-the-tide-falls">this poem</a> and <a href="https://www.youtube.com/watch?v=wrLDoitgYp4&list=FLT1K0Lc1vrCikcPdNI6spCA&index=74&ab_channel=YUNGRISK">this video</a> and <a href="https://www.youtube.com/watch?v=RMwR1kXuGzk&list=PLaPy9ZmwmJ3LbbYjygXVGYIQ-Uv8DuUAQ&index=3&ab_channel=EvanBertram">this song</a>.
 				</p>
 			</div>
