e4d9b32e7d373e87c620fee2388aba7d9cb60b9c
landon <laaker@equinix.com>
2023-06-04 16:44:16 -0700

    more edits

--- src/About.js
+++ src/About.js
@@ -8,7 +8,9 @@ export default function About() {
 				<img alt="image of a good software developer." className="selfPortrait" src="./img/family.png"/>
 				<h2 className="articleHeading">Who I Am</h2>
 				<p className="aboutText">
-					My name is Landon Aaker and I have been developing software for the past three years, professionally for the past year and a half. I have professional experience with NodeJS, Java and more. I also like coding in C#, and Python, and using Debian Linux distros. I love all things IT and I look forward towards creating solutions for you.
+				I am a software developer a Bachelor's degree in computer science and professional experience as a back-end developer. I specialize in backend web development, particularly in NodeJS and Java. At Equinix, a global server colocation company, I played a key role in large-scale integration tasks, streamlining inventory management through robust backend systems. Additionally, my experience at the Conference Exchange sharpened my problem-solving skills and allowed me to modernize legacy systems to meet industry standards.
+				Beyond my technical expertise, I possess strong negotiation and communication skills developed during my six years of teaching high school. This combination of technical proficiency and interpersonal abilities enables me to effectively understand client needs, explore solutions, and collaborate with fellow developers to improve software offerings.
+				For full resume, references, letters of req, questions or anything else, feel free to email me at landonaaker@gmail.com
 				</p>
 				<p className="aboutText">
 					When I am not programming, I am often reading old books, spending time with my baby, or planning out my next Dungeon World (like D&D but better) campaign.
